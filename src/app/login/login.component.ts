acbcff8d155f454c145daab1d528de7c45e8a31d
LyEs <lyzz.skate@gmail.com>
2024-03-04 10:56:55 -0600

    Model, Service empleados

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { UserModel } from '../../models/user';
 import  { LoginModel } from '../model/login';
 import { UserService } from '../services/user/user.service';
-
+import { EmpleadoModel } from '../model/empleado';
+import { Empleadoservice } from '../services/empleado/empleado.service';
 /* SWEET ALERT */
 import Swal from 'sweetalert2';
 
@@ -21,23 +22,57 @@ const Toast = Swal.mixin({
 export class LoginComponent {
 
   UserModel = new UserModel();
+  EmpleadoModel = new EmpleadoModel();
   LoginModel =  new LoginModel();
   /*email: string;*/
-  constructor(public UserService: UserService) {}
+  constructor(public UserService: UserService, public EmpleadoService: Empleadoservice) {}
 
   arrUser:any[] = [];
   users: any;
   idUser: string = "";
   arraNewUser: any[] = [];
+  arrEmpleado:any[] = [];
+  empleados: any;
+  idEmpleado: string = "";
+  arrNewEmpleado: any[] = [];
 
   ngOnInit(): void {
-    this.getUser();
-    this.arrUser = [];
+    this.getEmpleado();
+    this.arrEmpleado = [];
   }
 
   login() {
     console.log(this.UserModel._id);
     console.log(this.UserModel.strName);
+  }
+
+  getEmpleado() {
+    this.arrEmpleado = [];
+    this.EmpleadoService.getEmpleados().then((empleado: any) => {
+      
+      this.empleados = empleado;
+      
+      for (const empleado of this.empleados){
+        let element = [
+
+          empleado.name.replace(/\:null/gi,':""'),
+          empleado.position.replace(/\:null/gi,':""'),
+          empleado.office.replace(/\:null/gi,':""'),
+          empleado.salary,
+          empleado.blnActivo ? 'Sí' : 'No',
+        ];
+        this.arrEmpleado.push(element);
+        this.arrNewEmpleado = this.arrEmpleado;
+      }
+    }).catch((err: any) => {
+      
+      Toast.fire({
+            icon: 'error',
+            title: err.error
+            });
+      this.empleados = [];
+      
+    });
   }
 
   getUser() {
